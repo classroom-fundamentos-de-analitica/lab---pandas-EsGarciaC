979fafbc2c615c88b53893a9259d5a653c1b7f9a
EsGarciaC <esgarciac00@gmail.com>
2024-02-08 11:38:32 -0500

    Fixed Q#3

--- preguntas.py
+++ preguntas.py
@@ -50,7 +50,7 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    return tbl0["_c1"].value_counts(sort=True)
+    return tbl0["_c1"].value_counts().sort_index()
 
 
 def pregunta_04():
